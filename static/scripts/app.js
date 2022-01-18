9bbe52bee15eff1cfc24143be80bb6714630dda9
christoller <tollernz@gmail.com>
2022-01-18 21:56:52 +1100

    Update CSS and minor layout changes

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -7,13 +7,11 @@ let i;
 for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener('click', function () {
         this.classList.toggle('active');
-        var content = this.nextElementSibling;
+        let content = this.nextElementSibling;
         if (content.style.display === 'block') {
             content.style.display = 'none';
-            console.log('if');
         } else {
             content.style.display = 'block';
-            console.log('else');
         }
     });
 }
